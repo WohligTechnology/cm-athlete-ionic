10abd40953a40e1643096d65ff6334da2e936871
TusharSachde <tushar@wohlig.com>
2016-11-02 13:10:39 +0530

    Chart => Analytics

--- www/js/app.js
+++ www/js/app.js
@@ -7,371 +7,371 @@
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.styleBlackTranslucent();
-        }
-    });
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleBlackTranslucent();
+    }
+  });
 })
 
 .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
+  $stateProvider
 
-        .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'AppCtrl'
-    })
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
 
-    .state('app.profile', {
-        url: '/profile',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/profile.html',
-                controller: 'ProfileCtrl'
-            }
-        }
-    })
+  .state('app.profile', {
+    url: '/profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/profile.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
 
-    .state('login', {
-        url: '/login',
-        templateUrl: 'templates/login.html',
-        controller: 'LoginCtrl'
-    })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
 
-    .state('registration', {
-        url: '/registration',
-        templateUrl: 'templates/registration.html',
-        controller: 'RegistrationCtrl'
-    })
+  .state('registration', {
+    url: '/registration',
+    templateUrl: 'templates/registration.html',
+    controller: 'RegistrationCtrl'
+  })
 
-    .state('forgot-password', {
-        url: '/forgot-password',
-        templateUrl: 'templates/forgot-password.html',
-        controller: 'ForgotPasswordCtrl'
-    })
+  .state('forgot-password', {
+    url: '/forgot-password',
+    templateUrl: 'templates/forgot-password.html',
+    controller: 'ForgotPasswordCtrl'
+  })
 
-    .state('app.edit-profile', {
-        url: '/edit-profile',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/profile-edit.html',
-                controller: 'EditProfileCtrl'
-            }
-        }
-    })
+  .state('app.edit-profile', {
+    url: '/edit-profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/profile-edit.html',
+        controller: 'EditProfileCtrl'
+      }
+    }
+  })
 
-    .state('app.blog', {
-        url: '/blog',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/blog.html',
-                controller: 'BlogCtrl'
-            }
-        }
-    })
+  .state('app.blog', {
+    url: '/blog',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/blog.html',
+        controller: 'BlogCtrl'
+      }
+    }
+  })
 
-    .state('app.blogdetail', {
-        url: '/blog/detail',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/blog-detail.html',
-                controller: 'BlogDetailCtrl'
-            }
-        }
-    })
+  .state('app.blogdetail', {
+    url: '/blog/detail',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/blog-detail.html',
+        controller: 'BlogDetailCtrl'
+      }
+    }
+  })
 
-    .state('app.chat', {
-        url: '/chat',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/chat.html',
-                controller: 'ChatCtrl'
-            }
-        }
-    })
+  .state('app.chat', {
+    url: '/chat',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/chat.html',
+        controller: 'ChatCtrl'
+      }
+    }
+  })
 
-    .state('app.chatdetail', {
-        url: '/chat/detail',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/chat-detail.html',
-                controller: 'ChatDetailCtrl'
-            }
-        }
-    })
+  .state('app.chatdetail', {
+    url: '/chat/detail',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/chat-detail.html',
+        controller: 'ChatDetailCtrl'
+      }
+    }
+  })
 
-    .state('app.chat-group', {
-        url: '/chat-group',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/chat-group.html',
-                controller: 'ChatGroupCtrl'
-            }
-        }
-    })
+  .state('app.chat-group', {
+    url: '/chat-group',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/chat-group.html',
+        controller: 'ChatGroupCtrl'
+      }
+    }
+  })
 
-    .state('app.coach', {
-        url: '/coach',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/coach.html',
-                controller: 'CoachCtrl'
-            }
-        }
-    })
+  .state('app.coach', {
+    url: '/coach',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/coach.html',
+        controller: 'CoachCtrl'
+      }
+    }
+  })
 
-    .state('app.coach-detail', {
-        url: '/coach/detail',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/coach-detail.html',
-                controller: 'CoachDetailCtrl'
-            }
-        }
-    })
+  .state('app.coach-detail', {
+    url: '/coach/detail',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/coach-detail.html',
+        controller: 'CoachDetailCtrl'
+      }
+    }
+  })
 
-    .state('app.charts', {
-        url: '/charts',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/charts.html',
-                controller: 'ChartsCtrl'
-            }
-        }
-    })
+  .state('app.analytics', {
+    url: '/analytics',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/analytics.html',
+        controller: 'AnalyticsCtrl'
+      }
+    }
+  })
 
-    .state('app.competition-create', {
-        url: '/competition-create',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/competition-create.html',
-                controller: 'CompetitionCreateCtrl'
-            }
-        }
-    })
+  .state('app.competition-create', {
+    url: '/competition-create',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/competition-create.html',
+        controller: 'CompetitionCreateCtrl'
+      }
+    }
+  })
 
-    .state('app.competition-detail', {
-        url: '/competition-detail',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/competition-create.html',
-                controller: 'CompetitionDetailCtrl'
-            }
-        }
-    })
+  .state('app.competition-detail', {
+    url: '/competition-detail',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/competition-create.html',
+        controller: 'CompetitionDetailCtrl'
+      }
+    }
+  })
 
-    .state('app.injuries', {
-        url: '/injuries',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/injuries.html',
-                controller: 'InjuriesCtrl'
-            }
-        }
-    })
-
-
-    .state('app.injuries-create', {
-        url: '/injuries-create',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/injuries-create.html',
-                controller: 'InjuriesCreateCtrl'
-            }
-        }
-    })
-
-    .state('app.injuries-detail', {
-        url: '/injuries/detail',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/injuries-create.html',
-                controller: 'InjuriesDetailCtrl'
-            }
-        }
-    })
+  .state('app.injuries', {
+    url: '/injuries',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/injuries.html',
+        controller: 'InjuriesCtrl'
+      }
+    }
+  })
 
 
-    .state('app.search-coaches', {
-        url: '/search-coaches',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/search-coaches.html',
-                controller: 'SearchCoachesCtrl'
-            }
-        }
-    })
+  .state('app.injuries-create', {
+    url: '/injuries-create',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/injuries-create.html',
+        controller: 'InjuriesCreateCtrl'
+      }
+    }
+  })
 
-    .state('app.search-coaches-detail', {
-        url: '/search-coaches/detail',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/search-coaches-detail.html',
-                controller: 'SearchCoachesDetailCtrl'
-            }
-        }
-    })
+  .state('app.injuries-detail', {
+    url: '/injuries/detail',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/injuries-create.html',
+        controller: 'InjuriesDetailCtrl'
+      }
+    }
+  })
 
-    .state('app.notifications', {
-        url: '/notifications',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/notifications.html',
-                controller: 'NotificationsCtrl'
-            }
-        }
-    })
 
-    .state('app.training-diary', {
-        url: '/training-diary',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/training-diary.html',
-                controller: 'TrainingDiaryCtrl'
-            }
-        }
-    });
+  .state('app.search-coaches', {
+    url: '/search-coaches',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search-coaches.html',
+        controller: 'SearchCoachesCtrl'
+      }
+    }
+  })
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/login');
+  .state('app.search-coaches-detail', {
+    url: '/search-coaches/detail',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search-coaches-detail.html',
+        controller: 'SearchCoachesDetailCtrl'
+      }
+    }
+  })
+
+  .state('app.notifications', {
+    url: '/notifications',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/notifications.html',
+        controller: 'NotificationsCtrl'
+      }
+    }
+  })
+
+  .state('app.training-diary', {
+    url: '/training-diary',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/training-diary.html',
+        controller: 'TrainingDiaryCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
 })
 
 .filter('ageConvert', function () {
-    function calculateAge(birthday) { // birthday is a date
-        var ageDifMs = Date.now() - birthday.getTime();
-        var ageDate = new Date(ageDifMs); // miliseconds from epoch
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
+  function calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
 
-    function monthDiff(d1, d2) {
-        if (d1 < d2) {
-            var months = d2.getMonth() - d1.getMonth();
-            return months <= 0 ? 0 : months;
-        }
-        return 0;
+  function monthDiff(d1, d2) {
+    if (d1 < d2) {
+      var months = d2.getMonth() - d1.getMonth();
+      return months <= 0 ? 0 : months;
     }
-    return function (birthdate) {
-        var age = calculateAge(birthdate);
-        if (age === 0)
-            return monthDiff(birthdate, new Date()) + ' months';
-        return age + ' years';
-    };
+    return 0;
+  }
+  return function (birthdate) {
+    var age = calculateAge(birthdate);
+    if (age === 0)
+      return monthDiff(birthdate, new Date()) + ' months';
+    return age + ' years';
+  };
 })
 
 .filter('truncate', function () {
-    return function (text, length, end) {
-        if (isNaN(length)) {
-            length = 10;
-        }
+  return function (text, length, end) {
+    if (isNaN(length)) {
+      length = 10;
+    }
 
-        if (end === undefined) {
-            end = '...';
-        }
+    if (end === undefined) {
+      end = '...';
+    }
 
-        if (text.length <= length || text.length - end.length <= length) {
-            return text;
-        } else {
-            return String(text).substring(0, length - end.length) + end;
-        }
-    };
+    if (text.length <= length || text.length - end.length <= length) {
+      return text;
+    } else {
+      return String(text).substring(0, length - end.length) + end;
+    }
+  };
 })
 
 .directive('readMore', function ($filter, $ionicScrollDelegate) {
-    return {
-        restrict: 'A',
-        scope: {
-            text: '=readMore',
-            labelExpand: '@readMoreLabelExpand',
-            labelCollapse: '@readMoreLabelCollapse',
-            limit: '@readMoreLimit'
-        },
-        transclude: true,
-        template: '<span ng-transclude ng-bind-html="text"></span><a href="javascript:;" class="read-more" ng-click="toggleReadMore()" ng-if="applyLimit" ng-bind="label"></a>',
-        link: function (scope /*, element, attrs */ ) {
+  return {
+    restrict: 'A',
+    scope: {
+      text: '=readMore',
+      labelExpand: '@readMoreLabelExpand',
+      labelCollapse: '@readMoreLabelCollapse',
+      limit: '@readMoreLimit'
+    },
+    transclude: true,
+    template: '<span ng-transclude ng-bind-html="text"></span><a href="javascript:;" class="read-more" ng-click="toggleReadMore()" ng-if="applyLimit" ng-bind="label"></a>',
+    link: function (scope /*, element, attrs */ ) {
 
-            var originalText = scope.text;
-            scope.applyLimit = false;
-            if (scope.text.length >= scope.limit) {
-                scope.applyLimit = true;
-            }
+      var originalText = scope.text;
+      scope.applyLimit = false;
+      if (scope.text.length >= scope.limit) {
+        scope.applyLimit = true;
+      }
 
-            scope.label = scope.labelExpand;
+      scope.label = scope.labelExpand;
 
-            scope.$watch('expanded', function (expandedNew) {
-                if (expandedNew) {
-                    scope.text = originalText;
-                    scope.label = scope.labelCollapse;
-                } else {
-                    scope.text = $filter('truncate')(originalText, scope.limit, '...');
-                    scope.label = scope.labelExpand;
-                }
-            });
-
-            scope.toggleReadMore = function () {
-                scope.expanded = !scope.expanded;
-                $ionicScrollDelegate.resize();
-            };
-
+      scope.$watch('expanded', function (expandedNew) {
+        if (expandedNew) {
+          scope.text = originalText;
+          scope.label = scope.labelCollapse;
+        } else {
+          scope.text = $filter('truncate')(originalText, scope.limit, '...');
+          scope.label = scope.labelExpand;
         }
-    };
+      });
+
+      scope.toggleReadMore = function () {
+        scope.expanded = !scope.expanded;
+        $ionicScrollDelegate.resize();
+      };
+
+    }
+  };
 })
 
 .directive('autoGrow', function ($window) {
-    return {
-        restrict: 'A',
-        link: function ($scope, $element, $attrs) {
-            $scope.attrs = {
-                rows: 1,
-                maxLines: 999
-            };
-            for (var i in $scope.attrs) {
-                if ($attrs[i]) {
-                    $scope.attrs[i] = parseInt($attrs[i]);
-                }
-            }
-            $scope.getOffset = function () {
-                var style = $window.getComputedStyle($element[0], null),
-                    props = ['paddingTop', 'paddingBottom'],
-                    offset = 0;
-
-                for (var i = 0; i < props.length; i++) {
-                    offset += parseInt(style[props[i]]);
-                }
-                return offset;
-            };
-            $scope.autogrowFn = function () {
-                var newHeight = 0,
-                    hasGrown = false;
-                if (($element[0].scrollHeight - $scope.offset) > $scope.maxAllowedHeight) {
-                    $element[0].style.overflowY = 'scroll';
-                    newHeight = $scope.maxAllowedHeight;
-                } else {
-                    $element[0].style.overflowY = 'hidden';
-                    $element[0].style.height = 'auto';
-                    newHeight = $element[0].scrollHeight - $scope.offset;
-                    hasGrown = true;
-                }
-                $element[0].style.height = newHeight + 'px';
-                return hasGrown;
-            };
-
-            $scope.offset = $scope.getOffset();
-            $scope.lineHeight = ($element[0].scrollHeight / $scope.attrs.rows) - ($scope.offset / $scope.attrs.rows);
-            $scope.maxAllowedHeight = ($scope.lineHeight * $scope.attrs.maxLines) - $scope.offset;
-
-            $element[0].addEventListener('input', $scope.autogrowFn);
-            if ($element[0].value !== '') {
-                $scope.autogrowFn();
-            }
+  return {
+    restrict: 'A',
+    link: function ($scope, $element, $attrs) {
+      $scope.attrs = {
+        rows: 1,
+        maxLines: 999
+      };
+      for (var i in $scope.attrs) {
+        if ($attrs[i]) {
+          $scope.attrs[i] = parseInt($attrs[i]);
         }
+      }
+      $scope.getOffset = function () {
+        var style = $window.getComputedStyle($element[0], null),
+          props = ['paddingTop', 'paddingBottom'],
+          offset = 0;
 
-    };
+        for (var i = 0; i < props.length; i++) {
+          offset += parseInt(style[props[i]]);
+        }
+        return offset;
+      };
+      $scope.autogrowFn = function () {
+        var newHeight = 0,
+          hasGrown = false;
+        if (($element[0].scrollHeight - $scope.offset) > $scope.maxAllowedHeight) {
+          $element[0].style.overflowY = 'scroll';
+          newHeight = $scope.maxAllowedHeight;
+        } else {
+          $element[0].style.overflowY = 'hidden';
+          $element[0].style.height = 'auto';
+          newHeight = $element[0].scrollHeight - $scope.offset;
+          hasGrown = true;
+        }
+        $element[0].style.height = newHeight + 'px';
+        return hasGrown;
+      };
+
+      $scope.offset = $scope.getOffset();
+      $scope.lineHeight = ($element[0].scrollHeight / $scope.attrs.rows) - ($scope.offset / $scope.attrs.rows);
+      $scope.maxAllowedHeight = ($scope.lineHeight * $scope.attrs.maxLines) - $scope.offset;
+
+      $element[0].addEventListener('input', $scope.autogrowFn);
+      if ($element[0].value !== '') {
+        $scope.autogrowFn();
+      }
+    }
+
+  };
 });
