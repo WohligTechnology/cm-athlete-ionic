92aaf28dae2e89fbd9c04d0275efe6b5f44adbc4
TusharSachde <tushar@wohlig.com>
2016-11-26 15:24:47 +0530

    Restrict Paste

--- www/js/app.js
+++ www/js/app.js
@@ -388,6 +388,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   };
 })
 
+.directive('noPaste', function ($filter, $ionicScrollDelegate) {
+  return {
+    scope: {},
+    link: function (scope, element) {
+      element.on('cut copy paste', function (event) {
+        event.preventDefault();
+      });
+    }
+  };
+})
 
 .filter('uploadpath', function () {
   return function (input, width, height, style) {
