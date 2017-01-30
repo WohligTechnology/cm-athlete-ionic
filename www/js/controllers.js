525f84be700da7a6c8bf5e9f5dd7cac1d7f64c32
Harsh Thakkar <harshthakkar46@gmail.com>
2017-01-30 17:58:40 +0530

    done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,5 @@
-angular.module('starter.controllers', ['starter.services', 'checklist-model', 'chart.js', 'ui.calendar', 'ngCordova'])
-  // angular.module('starter.controllers', ['starter.services', 'checklist-model', 'ui.calendar', 'ngCordova'])
+// angular.module('starter.controllers', ['starter.services', 'checklist-model', 'chart.js', 'ui.calendar', 'ngCordova'])
+  angular.module('starter.controllers', ['starter.services', 'checklist-model', 'ui.calendar', 'ngCordova'])
 
 .controller('LoadingCtrl', function ($scope, $ionicModal, $timeout, $state, $rootScope, MyServices, $ionicHistory) {
   $scope.loadingData = MyServices.getUser();
